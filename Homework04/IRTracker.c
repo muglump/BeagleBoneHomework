a0a61407b3bf0daa737986640c273f9df143fcca
muglump <korahel@gmail.com>
2013-09-27 17:28:02 -0400

    Finished problem 1

--- Homework04/IRTracker.c
+++ Homework04/IRTracker.c
@@ -2,72 +2,109 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h> 
-#include <signal.h> 
-#include "gpio.h"
+#include <signal.h>
+#include <poll.h> 
+#include "gpio-utils.h"
 
 #define PIN_MUX_PATH "/sys/kernel/debug/omap_mux/"
-#define MAX_CYCLES = 20
+#define MAX_CYCLES 20
 
-
-void problemOne(unsigned int pins[4], int *pos){
-	clockwiseRotation(pins, &pos);
-	counterClockwiseRotation(pins, &pos);
-}
-
-void clockwiseRotation(unsigned int pins[4], int *pos){
-	switch(*pos){
+int clockwiseRotation(unsigned int pins[], int pos){
+	int checker = pos;
+	printf("Enter clockwise, with pos = %d \n", checker);
+	
+	
+	switch(checker){
 		case(0):
-			gpio_set_value(0, pin[0]);
-			gpio_set_value(1, pin[1]);
-			gpio_set_value(1, pin[2]);
-			gpio_set_value(0, pin[3]);
+			printf("Case 1 \n");
+			gpio_set_value(pins[0], 0);
+			gpio_set_value(pins[1], 1);
+			gpio_set_value(pins[2], 1);
+			gpio_set_value(pins[3], 0);
+			break;
 		case(1):
-			gpio_set_value(1, pin[0]);
-			gpio_set_value(1, pin[1]);
-			gpio_set_value(0, pin[2]);
-			gpio_set_value(0, pin[3]);
+			printf("Case 2 \n");
+			gpio_set_value(pins[0], 1);
+			gpio_set_value(pins[1], 1);
+			gpio_set_value(pins[2], 0);
+			gpio_set_value(pins[3], 0);
+			break;
+		case(2):
+			printf("Case 3 \n");
+			gpio_set_value(pins[0], 1);
+			gpio_set_value(pins[1], 0);
+			gpio_set_value(pins[2], 0);
+			gpio_set_value(pins[3], 1);
+			break;
 		case(3):
-			gpio_set_value(1, pin[0]);
-			gpio_set_value(0, pin[1]);
-			gpio_set_value(0, pin[2]);
-			gpio_set_value(1, pin[3]);
-		case(4):
-			gpio_set_value(0, pin[0]);
-			gpio_set_value(0, pin[1]);
-			gpio_set_value(1, pin[2]);
-			gpio_set_value(1, pin[3]);
+			printf("Case 4 \n");
+			gpio_set_value(pins[0], 0);
+			gpio_set_value(pins[1], 0);
+			gpio_set_value(pins[2], 1);
+			gpio_set_value(pins[3], 1);
+			break;
+		default:
+			printf("Fell \n");
+			break;
 		
+
 	}
-	delay(50);
-	*pos = (*pos+1)%4; 	
+	printf("Reached the botton, value: %d\n", checker);
+	sleep(1);
+	return(checker+1)%4; 	
 }
 
-void counterClockwiseRotation(unsigned int pin[4], int *pos){
-	switch(*pos){
+int counterClockwiseRotation(unsigned int pins[], int pos){
+	int checker = pos;
+	printf("Enter counterclockwise, with pos = %d \n", checker);
+	switch(checker){
 		case(0):
-			gpio_set_value(0, pin[1]);
-			gpio_set_value(1, pin[2]);
-			gpio_set_value(1, pin[2]);
-			gpio_set_value(0, pin[4]);
+			printf("Case 1B \n");
+			gpio_set_value(pins[0], 1);
+			gpio_set_value(pins[1], 0);
+			gpio_set_value(pins[2], 0);
+			gpio_set_value(pins[3], 1);
+			break;
 		case(1):
-			gpio_set_value(0, pin[1]);
-			gpio_set_value(0, pin[2]);
-			gpio_set_value(1, pin[3]);
-			gpio_set_value(1, pin[4]);
+			printf("Case 2B \n");
+			gpio_set_value(pins[0], 1);
+			gpio_set_value(pins[1], 1);
+			gpio_set_value(pins[2], 0);
+			gpio_set_value(pins[3], 0);
+			break;
+		case(2):
+			printf("Case 3B \n");
+			gpio_set_value(pins[0], 0);
+			gpio_set_value(pins[1], 1);
+			gpio_set_value(pins[2], 1);
+			gpio_set_value(pins[3], 0);
+			break;
 		case(3):
-			gpio_set_value(1, pin[1]);
-			gpio_set_value(0, pin[2]);
-			gpio_set_value(0, pin[3]);
-			gpio_set_value(1, pin[4]);
-		case(4):
-			gpio_set_value(1, pin[1]);
-			gpio_set_value(1, pin[2]);
-			gpio_set_value(0, pin[3]);
-			gpio_set_value(0, pin[4]);
+			printf("Case 4B \n");
+			gpio_set_value(pins[0], 0);
+			gpio_set_value(pins[1], 0);
+			gpio_set_value(pins[2], 1);
+			gpio_set_value(pins[3], 1);
+			break;
 	}
-	delay(50);
-	*pos = (*pos-1)%4; 	
+	sleep(1);
+	return (checker + 1)%4; 	
 }
+
+
+int problemOne(unsigned int pins[], int pos){
+	printf("Entering Problem one with position %d\n", pos);
+	int i =0;
+	for(i = 0; i < 20; i++){
+		pos = clockwiseRotation(pins, pos);
+	}
+	printf("After rotating clockwise pos is, %d \n", pos);
+	for(i = 0; i < 20; i++){
+		pos = counterClockwiseRotation(pins, pos);
+	}
+	return pos;
+}
+
 
 int main(int argc, char *argv[]){
 	//The gpio port numbers needed to drive the motor
@@ -77,14 +114,17 @@ int main(int argc, char *argv[]){
 	unsigned int gpio4 = 51;
 	unsigned int gpio5 = 15;
 	unsigned int gpios[4] = {gpio1, gpio2, gpio3, gpio4};
-	int behavior = argv[1]
+	int gpio_fd1, gpio_fd2, gpio_fd3, gpio_fd4, gpio_fd5;
+	int behavior = atoi(argv[1]);
 	int rc;
 	int len;
+	int nfds = 2;
 	//Port numbers for the analog in
-	char[] AnalogIn1 = "AIN0";
-	char[] AnalogIn2 = "AIN1";
+	char AnalogIn1[] = "AIN0";
+	char AnalogIn2[] = "AIN1";
 	char buf[64];
 	//Arrays to hold the values we get back from the sensors
+	struct pollfd fdset[2];
 	int Sensor1_val;
 	int Sensor2_val;
 	//int Sensor_sum;
@@ -105,6 +145,8 @@ int main(int argc, char *argv[]){
 	gpio_set_dir(gpio4, "out");
 	gpio_set_dir(gpio5, "in");
 	
+	gpio_set_edge(gpio5, "rising");
+	
 	gpio_fd1 = gpio_fd_open(gpio1, O_RDONLY);
 	gpio_fd2 = gpio_fd_open(gpio2, O_RDONLY);
 	gpio_fd3 = gpio_fd_open(gpio3, O_RDONLY);
@@ -116,15 +158,15 @@ int main(int argc, char *argv[]){
 	int minPosition = 0;
 	int Sensor_avg = 0;
 	
-	struct pollfd fdset[2];
+	
 	//Wait for the start button to be pushed before we acutally start doing anything
 	printf("Press the start button to begin the program \n");
-	
+	len = read(gpio_fd5, buf, 64);
 	while(1){
 		memset((void*)fdset, 0, sizeof(fdset));
-		fdest[0].fd = STDIN_FILENO;
-		fdset[0]events = POLLIN;
-		fdset[1].fd = gpio_fd;
+		fdset[0].fd = STDIN_FILENO;
+		fdset[0].events = POLLIN;
+		fdset[1].fd = gpio_fd5;
 		fdset[1].events = POLLPRI;
 		
 		rc = poll(fdset, nfds, 3000);
@@ -135,13 +177,11 @@ int main(int argc, char *argv[]){
 		if(fdset[1].revents & POLLPRI){
 			lseek(fdset[1].fd, 0, SEEK_SET);
 			len = read(fdset[1].fd, buf, 64);
-			problemOne(gpios, &pos);
-		}
-		if(fdset[0].revents & POLLIN){
-			(void) read(fdset[0].fd, buf, 1);
+			printf("Triggered \n");
+			pos = problemOne(gpios, pos);
 		}
 		
-		fflush(stdout);
+		
 	}
 	
 	
@@ -151,12 +191,12 @@ int main(int argc, char *argv[]){
 	
 	
 	
-	printf("Begining our steps to attempt to come up with ")
+	printf("Begining our steps to attempt to come up with ");
 	for(cycle = 0; cycle <= MAX_CYCLES; cycle++){
 		printf("One cycle number: %d", cycle);
 		Sensor1_val = analogIn(AnalogIn1);
 		Sensor2_val = analogIn(AnalogIn2);
-		Sensor_avg = (Sensor1_val[cycle] + Sensor2_val[cycle])/ 2;
+		Sensor_avg = (Sensor1_val + Sensor2_val)/ 2;
 		if(Sensor_avg < minValue){
 			minValue = Sensor_avg;
 			minPosition = cycle;
